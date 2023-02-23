c2cd77acecb6ab8bd699d3dc8386af06d9f902c3
Seemo0 <medouallal08@gmail.com>
2023-02-23 15:40:19 +0100

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import DarkModeBtn from "./DarkModeBtn";
 export default function Header() {
   return (
     <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
-      <div className="fkex">
+      <div className="flex ">
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
