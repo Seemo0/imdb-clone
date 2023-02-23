c99f965efd96311b6b7f0d0a61d2dc5db5e08ffc
Seemo0 <medouallal08@gmail.com>
2023-02-23 15:58:48 +0100

    add navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import Providers from "./Providers";
 
@@ -9,7 +10,12 @@ export default function RootLayout({ children }) {
         <Providers>
           {/* Header */}
           <Header />
+
+          {/* Navbar */}
+          <Navbar />
+
           {children}
+          
         </Providers>
       </body>
     </html>
