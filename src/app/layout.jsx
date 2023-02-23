90a2e40e2ce9e6340857b2319adaa8aa07b65d63
Seemo0 <medouallal08@gmail.com>
2023-02-23 01:53:30 +0100

    add darkmode

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,17 @@
-import Header from '@/components/Header';
-import './globals.css'
+import Header from "@/components/Header";
+import "./globals.css";
+import Providers from "./Providers";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* Header */}
-      <Header />
-
-      <body>{children}</body>
+      <body>
+        <Providers>
+          {/* Header */}
+          <Header />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return <main className="text-3xl font-bold underline">
-    
+    hello
   </main>;
 }
