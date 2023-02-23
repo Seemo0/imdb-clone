882c959d51ba6025941d05ae7a568af3c5a9aa3e
Seemo0 <medouallal08@gmail.com>
2023-02-24 00:45:39 +0100

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,30 @@
+export const dynamic = "force-dynamic";
 
-export default function Home() {
-  return <main className="text-3xl font-bold underline">
-    hello
-  </main>;
+import Resaults from "@/components/Resaults";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const search = searchParams.search || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      search === "fetchTopRated" ? "movie/top_rated" : "trending/movie/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+
+  return (
+    <div>
+      <Resaults results={results} />
+    </div>
+  );
 }
