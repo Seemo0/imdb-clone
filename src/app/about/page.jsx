3e1af1c2ff3ac581067ccf61e01ff348165ee55b
Seemo0 <mohamedouallal08@gmail.com>
2023-07-15 00:50:35 +0100

    changes delete p3

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,16 +7,6 @@ export default function About() {
         enthusiasts who have come together to create a one-stop destination for
         all your movie-related needs.
       </p>
-
-      <p>
-        Our website is designed to provide you with a comprehensive database of
-        movies from all around the world, along with the latest news, reviews,
-        and trailers. Our movie database is constantly updated with new
-        releases, ensuring that you have access to the latest and greatest in
-        the world of cinema. You can search for movies by title, director,
-        actor, genre, or release date, making it easy to find the perfect movie
-        for any occasion.
-      </p>
     </div>
   );
 }
